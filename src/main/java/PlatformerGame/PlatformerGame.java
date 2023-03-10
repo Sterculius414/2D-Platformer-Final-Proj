e2b560fd037d7385df67230a09a7e4bef9cfeb75
Sterculius <cseney414@mycr.redwoods.edu>
2023-03-09 20:59:23 -0800

    Added stuff

---
+++ src/main/java/PlatformerGame/PlatformerGame.java
@@ -0,0 +1,15 @@
+package PlatformerGame;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class PlatformerGame extends GameApplication {
+
+@Override
+protected void initSettings(GameSettings settings) {
+}
+
+public static void main(String[] args) {
+launch(args);
+    }
+}
